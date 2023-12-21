b482ccf5f9d1a9c982505509a3e81c75d09db9be
Florian Dufly <duflyflorian@gmail.com>
2023-12-21 12:01:24 +0100

    update page order

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from "react";
 import Accueil from './pages/Accueil';
+import Graph from './pages/Graph';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import DragDrop from './components/dragandrop';
 
@@ -9,8 +10,8 @@ export class App extends Component {
     return (
       <Routes>
         <Route path="/" element={<Navigate to="/Accueil" />} />
-        <Route path="/Accueil" element={<Accueil />} />
-        <Route path="/Drag" element={<DragDrop/>} />
+        <Route path="/Accueil" element={<DragDrop />} />
+        <Route path="/Graph" element={<Graph />} />
       </Routes>
     );
   }
